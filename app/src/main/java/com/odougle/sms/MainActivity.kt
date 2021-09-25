7c119ac5fb74ac32f72520d8f8fdd5de99ff7095
douguizilla <douglasgommez@gmail.com>
2021-09-25 18:14:56 -0300

    set check self permissions

--- app/src/main/java/com/odougle/sms/MainActivity.kt
+++ app/src/main/java/com/odougle/sms/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.odougle.sms
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.odougle.sms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val permissions = arrayOf(
+            android.Manifest.permission.READ_SMS,
+            android.Manifest.permission.RECEIVE_SMS
+        )
+        val hasPermissions = permissions.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+        if(!hasPermissions){
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        }
     }
 }
